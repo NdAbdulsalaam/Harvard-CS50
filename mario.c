ad2acbd9520626a4573726fb06938e0dba1c9e4a
unknown <olaitansalaam2012@gmail.com>
2022-12-25 08:17:09 +0100

    build the blocks in mario

---
+++ mario.c
@@ -0,0 +1,48 @@
+// Import libraries
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt pyramid height
+    int n;
+
+    // Accept height between 1 and 8
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    // For height
+    for (int i = 1; i <= n; i++)
+    {
+        // For columns
+        int s = n - i;
+        //  left space
+        for (int j = 0; j < s; j++)
+        {
+            printf(" ");
+        }
+        // Left block
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        // Middle space
+        for (int j = 0; j < 2; j++)
+        {
+            printf(" ");
+        }
+
+        // Right block
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        // Move to newline at the end of each column
+        printf("\n");
+    }
+
+}
